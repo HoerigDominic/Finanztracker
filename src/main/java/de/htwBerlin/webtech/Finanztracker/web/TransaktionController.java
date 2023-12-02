e2fecfba3c90378cded9bb9f1e471aee4f3d82bf
HoerigDominic <s0578252@htw-berlin.de>
2023-12-02 20:11:46 +0100

    PutMapping implementiert, Änderung Delete- und Getmapping vorgenommen

--- src/main/java/de/htwBerlin/webtech/Finanztracker/web/TransaktionController.java
+++ src/main/java/de/htwBerlin/webtech/Finanztracker/web/TransaktionController.java
@@ -3,24 +3,31 @@ package de.htwBerlin.webtech.Finanztracker.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class TransaktionController {
 
     @Autowired
-    TransaktionService service;
+    private TransaktionService service;
+
     @PostMapping("/transaktionen")
     public Transaktion createTransatkion(@RequestBody Transaktion transaktion) {
-        return service.save(transaktion);
+        return service.create(transaktion);
     }
 
     @GetMapping("/transaktionen/{id}")
-    public Transaktion getTransaktion(@PathVariable String id) {
-        Long transaktionId = Long.parseLong(id);
-        return service.get(transaktionId);
+    public Transaktion getTransaktion(@PathVariable Long id) {
+        return service.get(id);
     }
 
-   @DeleteMapping("/{id}")
-   public void deleteTransaktion(@PathVariable ("id") Long transaktionid) {
-       service.delete(transaktionid);
+    @PutMapping("/transaktionen/{id}")
+    public void updateTransaktion(@PathVariable Long id, @RequestBody Transaktion transaktion) {
+        service.update(id, transaktion);
+    }
+
+   @DeleteMapping("/transaktionen/{id}")
+   public void deleteTransaktion(@PathVariable Long id) {
+       service.delete(id);
    }
 }
