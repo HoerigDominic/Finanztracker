c8e7f2f4dcc4bb2c94b9bfed8e6e696ea79b6769
HoerigDominic <s0578252@htw-berlin.de>
2023-12-03 21:43:47 +0100

    WebConfig Klasse für CrossOrigin erstellt

---
+++ src/main/java/de/htwBerlin/webtech/Finanztracker/web/config/WebConfig.java
@@ -0,0 +1,19 @@
+package de.htwBerlin.webtech.Finanztracker.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/transaktionen")
+                .allowedMethods("*")
+                .allowedOrigins("http://localhost:8081");
+    }
+}
