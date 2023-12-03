27c09286adf6e396822d2afbca1b9f72bc9faf6c
HoerigDominic <s0578252@htw-berlin.de>
2023-12-03 14:21:58 +0100

    Von CRDU auf JPA Repository umgestellt

--- src/main/java/de/htwBerlin/webtech/Finanztracker/web/TransaktionRepository.java
+++ src/main/java/de/htwBerlin/webtech/Finanztracker/web/TransaktionRepository.java
@@ -1,8 +1,9 @@
 package de.htwBerlin.webtech.Finanztracker.web;
 
 import de.htwBerlin.webtech.Finanztracker.web.Transaktion;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransaktionRepository extends CrudRepository<Transaktion, Long> {}
+public interface TransaktionRepository extends JpaRepository<Transaktion, Long> {}
